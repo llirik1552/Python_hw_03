23dbec780bf7ed2136ca980b3a3f5f19dc9ba829
llirik1552 <llirik1552@gmail.com>
2025-05-05 23:09:01 +0300

    lesson6K

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,7 +1,8 @@
 import pytest
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture
@@ -13,8 +14,9 @@ def driver():
 
 @pytest.mark.usefixtures("driver")
 def test_fill_form(driver):
-    driver.get("https://bonigarcia.dev/selenium-"
-               "webdriver-java/slow-calculator.html")
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+    )
     element = driver.find_element(By.CSS_SELECTOR, "#delay")
     element.clear()
     driver.find_element(By.CSS_SELECTOR, "#delay").send_keys("45")
@@ -23,7 +25,9 @@ def test_fill_form(driver):
     driver.find_element(By.XPATH, "//span[text()='+']").click()
     driver.find_element(By.XPATH, "//span[text()='8']").click()
     driver.find_element(By.XPATH, "//span[text()='=']").click()
-    time.sleep(45)
+    WebDriverWait(driver, 46).until(EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, "screen"), "15"))
+    result = driver.find_element(By.CLASS_NAME, "screen").text
 
     result = driver.find_element(By.CSS_SELECTOR, ".screen").text
     assert int(result) == 15
